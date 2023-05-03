00245e29c7e1e198a679913baf77e76cd55744c4
rachio142 <111333904+rachio142@users.noreply.github.com>
2023-05-03 11:09:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)https://github.com/rachio142/zenas_web_Catalog/blob/main/streamlit_app.py
+df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
 # put the first column into a list
